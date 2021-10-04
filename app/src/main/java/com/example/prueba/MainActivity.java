1cb5863c730c5138c6e6de2e68b08887224fae01
joaquingb4 <joaquin_alonsogarcia@iesjoandaustria.org>
2021-10-04 19:36:45 +0200

    commit1

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -31,20 +31,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Login button created
         Button btnLogin = findViewById(R.id.btnSignin);
+        //Name and password fields created
         EditText txtUserName = findViewById(R.id.txtUserName);
         EditText txtPassword = findViewById(R.id.txtPassword);
+        //The textview created
         TextView lblLoginResult = findViewById(R.id.lblLoginResult);
+        lblLoginResult.setVisibility(View.INVISIBLE); //Textview invisible
 
-
+        //If we click on button login
         btnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if (txtUserName.getText().toString().equals("Alonso")){
+                //Check if the name and password fields are correct
+                if (txtUserName.getText().toString().equals("Alonso") && txtPassword.getText().toString().equals("1234")){
+
+                    lblLoginResult.setText("Successful login");//Set message on the textview
                     Log.i("Test","Successful login");
+
                 }else{
+                    lblLoginResult.setText("Wrong name or password");//Set message on the textview
                     Log.i("Test","Wrong name or password");
                 }
+                //Text view turn in visible
+                lblLoginResult.setVisibility(View.VISIBLE);
             }
         });
 
